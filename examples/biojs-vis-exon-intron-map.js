b6d227c5be5c715e9e096684dbca9aadc34ff2c3
ohalloranlab <dohalloran@users.noreply.github.com>
2018-07-23 08:58:36 -0400

    Update biojs-vis-exon-intron-map.js

--- examples/biojs-vis-exon-intron-map.js
+++ examples/biojs-vis-exon-intron-map.js
@@ -95,7 +95,7 @@ class exonIntronMap {
         scale() {
 
                 let margin = {
-                                top: 50,
+                                top: 0,
                                 right: 50,
                                 bottom: 0,
                                 left: 80
